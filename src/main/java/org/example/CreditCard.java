0e4cc358f19f57ceeee49fb1e18d05158d0ebac0
YanaKiryanova <yan573@yandex.ru>
2022-10-07 10:17:28 +0300

    Lesson6,1

---
+++ src/main/java/org/example/CreditCard.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class CreditCard {
+    int numberCreditCard;
+    int sum;
+
+    public CreditCard(){}
+
+    public CreditCard(int numberCreditCard, int sum) {
+        this.numberCreditCard = numberCreditCard;
+        this.sum = sum;
+    }
+    public void plus (int sum){
+        this.sum += sum;
+    }
+    public void minus (int sum){
+        this.sum -= sum;
+    }
+    public void conditionCard(){
+        System.out.printf("Кредитная карта %d, сумма счета составляет %d%n", this.numberCreditCard,this.sum);
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        CreditCard creditCard1 = new CreditCard(10001, 500);
+        CreditCard creditCard2 = new CreditCard(20002, 800);
+        CreditCard creditCard3 = new CreditCard(30003, 1500);
+
+        creditCard1.plus(550);
+        creditCard1.conditionCard();
+        creditCard2.plus(1500);
+        creditCard2.conditionCard();
+        creditCard3.minus(1000);
+        creditCard3.conditionCard();
+
+    }
+
+}
